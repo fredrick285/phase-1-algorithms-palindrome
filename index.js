e1d902465123db912db32e5a1ed4ee19fba5409c
Fredrick <fredrick.lokidor@student.moringaschool.com>
2022-06-18 10:39:33 +0300

    Done.

--- index.js
+++ index.js
@@ -1,13 +1,31 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  let palindrome='';
+  for(let i=word.lenght-1;i>0; i--){
+    palindrome+=word[i]
+  }
+  if(word===palindrome){
+    return true}
+  else{return false
+ }
 }
 
 /* 
   Add your pseudocode here
+  initialize empty string called palindrome
+  iterate over the input string 'word'
+  then append characters into the palindrome
+  if input is the same as the palindrome the return true
+  if not same then return false 
+
 */
 
 /*
   Add written explanation of your solution here
+  -I created an empty string and named it palindrome
+  -I iterated the input string in reverse while matching it with the palidrome
+  -After comparison I returned true when the comparisons are matching and returned false 
+   for those that didn't match
 */
 
 // You can run `node index.js` to view these console logs
